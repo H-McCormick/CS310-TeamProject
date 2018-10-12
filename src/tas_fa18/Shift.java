8066a87ca5138f85bfe5c7487e2f55f307d9fb4a
Chao Lin <Clin1@stu.jsu.edu>
2018-10-12 12:06:07 -0500

    In progress shift

--- src/tas_fa18/Shift.java
+++ src/tas_fa18/Shift.java
@@ -5,17 +5,62 @@
  */
 package tas_fa18;
 
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.LocalDate;
 /**
  *
  * @author Chao
  */
 public class Shift {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public int ID;
+    public String Description;
+    public Time Start;
+    public Time Stop;
+    public int Internal;
+    public int Graceperiod;
+    public int Dock;
+    public Time LunchStart;
+    public Time LunchStop;
+    public int LunchDecduct; 
+    
+    
+    public Shift(){
+        
+        ID= ID;
+        Description = "";
+        Start = new GregorianCalendar();
+        Stop = new GregorianCalendar();
+        Internal = 0;
+        Graceperiod=0;
+        Dock= 0;
+        LunchStart = new GregorianCalendar();
+        LunchStop =new GregorianCalendar();
+        LunchDecduct= 0;
+        
+}    
+    
+    public int getID(){
+        return ID;
+    }
+    
+    public String getDescription(){
+        return Description;
+    }
+    public GregorianCalendar getSatrt(){
+        return Start;
+    }
+    public GregorianCalendar getStop(){
+        return Stop;
+    }
+    public int getInternal(){
+        return Internal;
+    }
+    public int getGraceperiod(){
+        return Graceperiod;
     }
     
 }
+
