437986911e594d5c698609e2018f0f5cd3b04a7c
Chao Lin <Clin1@stu.jsu.edu>
2018-10-14 13:43:19 -0500

    shift

--- src/tas_fa18/Shift.java
+++ src/tas_fa18/Shift.java
@@ -8,7 +8,7 @@ package tas_fa18;
 import java.util.GregorianCalendar;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.time.LocalDate;
+import java.time.LocalTime;
 /**
  *
  * @author Chao
@@ -24,24 +24,25 @@ public class Shift {
     public int Dock;
     public Time LunchStart;
     public Time LunchStop;
-    public int LunchDecduct; 
+    public int LunchDeduct; 
     
     
-    public Shift(){
-        
-        ID= ID;
-        Description = "";
-        Start = new GregorianCalendar();
-        Stop = new GregorianCalendar();
-        Internal = 0;
-        Graceperiod=0;
-        Dock= 0;
-        LunchStart = new GregorianCalendar();
-        LunchStop =new GregorianCalendar();
-        LunchDecduct= 0;
-        
+    public Shift(int ID, String Description, Time Start, Time Stop, int Internal,
+            int Graceperiod, int Dock, Time LunchStart, Time LunchStop, 
+            int LunchDeduct){
+       
+        this.ID= ID;
+        this.Description = Description;
+        this.Start= Start;
+        this.Stop= Stop;
+        this.Internal= Internal;
+        this.Graceperiod= Graceperiod;
+        this.Dock= Dock;
+        this.LunchStart= LunchStart;
+        this.LunchStop= LunchStop;
+        this.LunchDeduct= LunchDeduct;
+                
 }    
-    
     public int getID(){
         return ID;
     }
@@ -49,18 +50,39 @@ public class Shift {
     public String getDescription(){
         return Description;
     }
-    public GregorianCalendar getSatrt(){
+   
+    public Time getStart(){
         return Start;
     }
-    public GregorianCalendar getStop(){
+    
+    public Time getStop(){
         return Stop;
     }
     public int getInternal(){
         return Internal;
     }
-    public int getGraceperiod(){
-        return Graceperiod;
+    public int getDock(){
+        return Dock;
+    }
+    public Time getLunchStart(){
+        return LunchStart;
+    }
+    public Time getLunchStop(){
+        return LunchStop;
+    }
+    public int getLunchDeduct(){
+        return LunchDeduct;
+    }
+    private String getHourMin(){
+        return Time.toString().substring(0, 5);
+    }
+    
+    public String toString(){
+            return Description + ": " + getHourMin(Start) + " - " + getHourMin(Stop) + " (510 minutes); Lunch: " + getHourMin(LunchStart) + " - " + getHourMin(LunchStop) + " (30 minutes)";
+            
     }
     
 }
+
+
 
